c726268bd8667bcea1ca4dd57db3feeefbc179b0
SerhiiChynin <sergey.chinin@gmail.com>
2023-01-05 21:04:04 +0200

    Finish_game

--- js/game.js
+++ js/game.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 const ground = new Image();
-// ground.src = "img/ground.png";
+ground.src = "img/ground.png";
 
 const foodImg = new Image();
 foodImg.src = "img/Food_2.png";
+
+const foodImg_2 = new Image();
+foodImg_2.src = "img/Food.png";
 
 let box = 32;
 
@@ -37,13 +40,20 @@ function direction(e) {
         dir = 'down';
 }
 
+function eatTail(head, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (head.x == arr[i].x && head.y == arr[i].y) clearInterval(game);
+    } 
+}
+
 
 
 function drawGame() {
      ctx.drawImage(ground, 0, 0)
      ctx.drawImage(foodImg, food.x, food.y)
+     ctx.drawImage(foodImg_2, 5, 5)
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = 'orange';
+        ctx.fillStyle = i == 0 ? 'orange' : 'yellowgreen';
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
@@ -55,7 +65,21 @@ function drawGame() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    if (snakeX == food.x && snakeY == food.y) {
+        score++;
+        food = {
+        x: Math.floor((Math.random() * 14 + 1)) * box,
+        y: Math.floor((Math.random() * 14 + 1)) * box,
+    }
+    } 
+    else {
     snake.pop();
+    }
+
+    if (snakeX < box || snakeX > box * 14
+        || snakeY < box || snakeY > box * 14) clearInterval(game);
+    
+
  
     if (dir == 'left') snakeX -= box;
     if (dir == 'right') snakeX += box;
@@ -67,9 +91,9 @@ function drawGame() {
         y: snakeY
     };
     
-
+    eatTail(newHead, snake);
     snake.unshift(newHead);
 }
 
 
-let game = setInterval(drawGame, 100);
+let game = setInterval(drawGame, 400);
